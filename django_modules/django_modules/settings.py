e0bf13fc5c35ce0cf965d73dd871d9e364563f19
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-09-04 23:46:57 +0100

    working on page reload with ajax

--- django_modules/django_modules/settings.py
+++ django_modules/django_modules/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'store',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_modules/store/views.py
+++ django_modules/store/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def say_hello(request):
-    return HttpResponse("hello from app!")
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Students
+from .serializers import studentSerializer
+
+
+class Get_students_List(APIView):
+    def get(self, request):
+        students = Students.objects.all()
+        serialized = studentSerializer(students, many=True)
+        return Response(serialized.data)
+
+
+def homepage(request):
+    return render(request, 'index.html')
